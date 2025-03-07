217b9d4ca4848bdca9c5fe189630600aa3882336
Jordi Alemany <jordia4@gmail.com>
2025-03-07 09:00:15 +0100

    crear primera parte del reloj. Pendientes de auditar en fichero de log

--- practica_1_reloj_express/src/app.js
+++ practica_1_reloj_express/src/app.js
@@ -1,12 +1,48 @@
 import Reloj from './reloj.js'
+import express from 'express'
+import fs from 'node:fs'
+import path from 'node:path'
 import picocolors from 'picocolors'
 
-const r1 = new Reloj("CASIO");
+const nombreReloj = process.argv[2] ?? "RELOJ";
+const reloj = new Reloj(nombreReloj);
 
-r1.start();
+const PORT = process.env.PORT ?? 3000;
+const ACTIONS = ['START','PAUSE','RESUME','INVERT','RESET']
 
-r1.eventEmitter.on('timeGoesBy', (reloj) => {
+const app = express();
+app.disable('x-powered-by');
 
+app.use((req, res, _next) => {
+
+   const ACTION = (req.originalUrl.slice(1)).toUpperCase();
+
+   if (req.method === "GET" && ACTIONS.includes(ACTION)){
+      
+      switch(ACTION){
+         case 'START': reloj.start(); break;
+         case 'PAUSE': reloj.pause(); break;
+         case 'RESUME': reloj.resume(); break;
+         case 'INVERT': reloj.invert(); break;
+         case 'RESET': reloj.reset(); break;
+      }
+
+      const logReloj = {
+         "nombre": `Reloj ${reloj.nombre}`,
+         "action": ACTION,
+         "estado": reloj.estado,
+         "totalSegundos": reloj.totalSegundos,
+         "entido": reloj.sentido
+      }
+
+      auditar(logReloj);
+      return res.status(200).json(logReloj);
+   }
+
+   return res.status(400).json({ mensaje: "Se debe utiizar el verbo GET y la acción permitida" });
+})
+
+reloj.eventEmitter.on('timeGoesBy', (reloj) => {
    const display = picocolors.blue(`${reloj.horas}:${reloj.minutos}:${reloj.segundos}`)
    const sentido = reloj.sentido === 1 ? picocolors.cyan("UP") : picocolors.red("DOWN")
    const estado = picocolors.yellow(reloj.estado)
@@ -14,6 +50,10 @@ r1.eventEmitter.on('timeGoesBy', (reloj) => {
    console.log(`${reloj.nombre}: ${display} ${sentido} [${estado}]`)
 });
 
-console.log("entramos")
+app.listen(PORT, () => {
+    console.log(picocolors.blue(`Servidor escuchando el puerto ${PORT}`));
+});
 
-
+function auditar(logReloj){
+   // TODO Auditar en el fichero de logs...
+}
